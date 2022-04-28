44fc87497591559f50d8375b06ea25a064caded5
Dwi Putra Bayu <74140729+dpbayu@users.noreply.github.com>
2022-04-28 12:24:51 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        first: '#ff6c0c',
+        secondary: '#fe806b',
+        third: '#e6e6e6',
+        forth: '#2e2b26',
+      }
+    },
+  },
+  plugins: [],
+}
