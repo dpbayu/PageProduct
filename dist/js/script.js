04cad2f61a7e7c74995e313495bc708260aa9b27
Dwi Putra Bayu <74140729+dpbayu@users.noreply.github.com>
2022-05-11 16:29:22 +0700

    initial commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -28,3 +28,18 @@ window.onclick = (event) => {
         document.body.classList.remove("disable-scroll")
     }
 };
+
+let addBtn = document.querySelector('#add');
+let subBtn = document.querySelector('#sub');
+let qty = document.querySelector('#qtyBox');
+
+addBtn.addEventListener('click', () => {
+    qty.value = parseInt(qty.value) + 1;
+});
+subBtn.addEventListener('click', () => {
+    if (qty.value <= 0) {
+        qty.value = 0;
+    } else {
+        qty.value = parseInt(qty.value) - 1;
+    }
+});
